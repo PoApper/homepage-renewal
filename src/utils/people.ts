15cace8605adbbd3f8a213e81ae4300188d61737
HyoJeong Yun <hodori@postech.ac.kr>
2024-06-14 22:52:02 +0900

    page - people

---
+++ src/utils/people.ts
@@ -0,0 +1,18 @@
+const people = [
+  { githubId: "github1", tag: "front-end" },
+  { githubId: "github2", tag: "back-end" },
+  { githubId: "github3", tag: "full-stack" },
+  { githubId: "github4", tag: "ios" },
+  { githubId: "github5", tag: "android" },
+  { githubId: "github6", tag: "ai" },
+  { githubId: "github7", tag: "security" },
+  { githubId: "github8", tag: "blockchain" },
+  { githubId: "github9", tag: "game" },
+  { githubId: "github10", tag: "etc" },
+  { githubId: "github11", tag: "etc" },
+  { githubId: "github12", tag: "etc" },
+];
+
+export default {
+  people,
+};
