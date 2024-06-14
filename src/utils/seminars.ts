3312d3123f2ee066b7e582255d7300ec1d06ad7d
HyoJeong Yun <hodori@postech.ac.kr>
2024-06-14 22:57:29 +0900

    page - seminar

---
+++ src/utils/seminars.ts
@@ -0,0 +1,35 @@
+const seminars = [
+  {
+    name: "Front-end",
+    url: "#",
+    images: [
+      "/public/seminar/html.png",
+      "/public/seminar/css.png",
+      "/public/seminar/react.png",
+    ],
+  },
+  {
+    name: "Back-end",
+    url: "https://github.com/PoApper/backend-seminar",
+    images: ["/public/seminar/nodejs.png", "/public/seminar/mysql.png"],
+  },
+  {
+    name: "Mobile",
+    url: "https://github.com/PoApper/flutter_seminar",
+    images: ["/public/seminar/dart.png", "/public/seminar/flutter.png"],
+  },
+  {
+    name: "NestJs",
+    url: "https://github.com/PoApper/nestjs-seminar",
+    images: ["/public/seminar/nestjs.png", "/public/seminar/jest.png"],
+  },
+  {
+    name: "PyTorch",
+    url: "https://github.com/PoApper/pytorch-seminar",
+    images: ["/public/seminar/pytorch.png"],
+  },
+];
+
+export default {
+  seminars,
+};
