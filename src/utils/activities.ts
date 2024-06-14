d77294de6a0e4840d5d05419f255bb7ff536a4cd
HyoJeong Yun <hodori@postech.ac.kr>
2024-06-15 00:49:54 +0900

    activities, projects - home section

---
+++ src/utils/activities.ts
@@ -0,0 +1,18 @@
+const activities = [
+  {
+    name: "Seminar",
+    description:
+      "포애퍼는 세미나를 통해 기술을 공유하고 교류하며 성장합니다. 누구나 세미나를 열 수 있고, 자유롭게 참가할 수 있습니다. 모든 세미나는 PoApper Archive로 관리되며 자유롭게 열람할 수 있습니다!",
+    iconUrl: "https://img.icons8.com/ios/250/training.png",
+  },
+  {
+    name: "Hackaton",
+    description:
+      "포애퍼는 매년 교내 해커톤을 개최하고 있습니다. 자유롭게 아이디어를 제시하고 개발 및 검증하며 마음껏 창의성을 발휘하는 포애퍼의 🎉Develop Festival🎊입니다.",
+    iconUrl: "https://img.icons8.com/ios/250/light-on.png",
+  },
+];
+
+export default {
+  activities,
+};
