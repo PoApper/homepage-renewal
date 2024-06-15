773d3f1be87315183c7ce48ba3d92ccf5db3425f
HyoJeong Yun <hodori@postech.ac.kr>
2024-06-15 14:00:49 +0900

    split junior and senior in people page

--- src/utils/people.ts
+++ src/utils/people.ts
@@ -1,4 +1,19 @@
-const people = [
+const junior = [
+  { githubId: "github1", tag: "front-end" },
+  { githubId: "github2", tag: "back-end" },
+  { githubId: "github3", tag: "full-stack" },
+  { githubId: "github4", tag: "ios" },
+  { githubId: "github5", tag: "android" },
+  { githubId: "github6", tag: "ai" },
+  { githubId: "github7", tag: "security" },
+  { githubId: "github8", tag: "blockchain" },
+  { githubId: "github9", tag: "game" },
+  { githubId: "github10", tag: "etc" },
+  { githubId: "github11", tag: "etc" },
+  { githubId: "github12", tag: "etc" },
+];
+
+const senior = [
   { githubId: "github1", tag: "front-end" },
   { githubId: "github2", tag: "back-end" },
   { githubId: "github3", tag: "full-stack" },
@@ -14,5 +29,6 @@ const people = [
 ];
 
 export default {
-  people,
+  junior,
+  senior
 };
