b88846711b6cc8fb1bd9d76d6bafc83b827a86fc
HyoJeong Yun <hodori@postech.ac.kr>
2024-06-15 15:24:18 +0900

    install package - rehype-pretty-code

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,19 @@ import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
+import rehypePrettyCode from 'rehype-pretty-code';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind(), react()]
+  integrations: [mdx({
+    syntaxHighlight: false,
+    rehypePlugins: [
+      [
+        rehypePrettyCode,
+        {
+          theme: 'catppuccin-latte',
+        },
+      ],
+    ],
+  }), tailwind(), react()]
 });
