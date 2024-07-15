cf62bb614d54454807effc0899219f7c76ee7c45
HyoJeong Yun <hodori@postech.ac.kr>
2024-07-15 22:18:32 +0900

    Dockerization (#2)
    * env
    
    * install package - @astrojs/node
    
    * Dockerfile
    
    * github action workflow
    
    * pull request template

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,21 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
 import rehypePrettyCode from 'rehype-pretty-code';
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx({
     syntaxHighlight: false,
-    rehypePlugins: [
-      [
-        rehypePrettyCode,
-        {
-          theme: 'catppuccin-latte',
-        },
-      ],
-    ],
-  }), tailwind(), react()]
+    rehypePlugins: [[rehypePrettyCode, {
+      theme: 'catppuccin-latte'
+    }]]
+  }), tailwind(), react()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
